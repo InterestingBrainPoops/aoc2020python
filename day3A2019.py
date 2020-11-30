50f4e28e3b524896b9a04cb744dcb61f8d9cc15b
InterestingBrainPoops <satasiyahet@gmail.com>
2020-11-30 15:52:16 -0800

    added a timer for when i keyboard interrupt

--- day3A2019.py
+++ day3A2019.py
@@ -1,5 +1,5 @@
 count = [0,0,0]
-import re
+import re,time,sys,os
 def getpath(poses):
     ret = [[0,0]]
     for x in range(len(poses)):
@@ -17,6 +17,7 @@ def getpath(poses):
                 ret[len(ret)-1][1] += 1
             #print(ret[len(ret)-1])
     return ret
+
 with open("./input.txt") as fp:
     for cnt, line in enumerate(fp):
         count[cnt] = line.split(',')
@@ -24,8 +25,21 @@ with open("./input.txt") as fp:
 #print(count)
 w1 = getpath(count[0])
 w2 = getpath(count[1])
-for x in range(len(w1)):
-    for y in range(len(w2)):
-        if(w1[x] == w2[y]):
-            print(w1[x])
+print(len(w2))
+unix_time_ms_2 = int(time.time() * 1000)
+def main2():
+    for x in range(len(w1)):
+        for y in range(len(w2)):
+            if(w1[x] == w2[y]):
+                print(w1[x])
+if __name__ == '__main__':
+    try:
+        main2()
+    except KeyboardInterrupt:
+        unix_time_ms_3 = int(time.time() * 1000)
+        print(unix_time_ms_3-unix_time_ms_2)
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
 #print(w2)
