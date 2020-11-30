e5844d900a2ab92d9fa3b7b1089f32cfa63507f0
InterestingBrainPoops <satasiyahet@gmail.com>
2020-11-30 15:53:06 -0800

    added another thing for when i keyboard interuppt

--- day3A2019.py
+++ day3A2019.py
@@ -27,8 +27,10 @@ w1 = getpath(count[0])
 w2 = getpath(count[1])
 print(len(w2))
 unix_time_ms_2 = int(time.time() * 1000)
+global z
 def main2():
     for x in range(len(w1)):
+        z += 1
         for y in range(len(w2)):
             if(w1[x] == w2[y]):
                 print(w1[x])
@@ -38,6 +40,7 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         unix_time_ms_3 = int(time.time() * 1000)
         print(unix_time_ms_3-unix_time_ms_2)
+        print(z)
         try:
             sys.exit(0)
         except SystemExit:
